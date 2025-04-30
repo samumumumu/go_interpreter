c63ce3c20d4d0ff2d786689958721354169d280c
fumi39 <fumi1014s@ezweb.ne.jp>
2025-05-01 07:32:37 +0900

    2章未実装だったところ

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -117,6 +117,8 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+	p.nextToken()
+	stmt.Value = p.parseExpression(LOWEST)
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
